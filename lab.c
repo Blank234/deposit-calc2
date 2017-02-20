3b820eb5181e660f9973ea9e6cf09c08db12bca0
Алымбаев Тимур Русланович <ip612s01@awp05.csc.local>
2017-02-20 09:02:47 +0700

    Srok 241-365

--- lab.c
+++ lab.c
@@ -33,6 +33,11 @@ if(sr<241 && 121<=sr)
 {  
 den=sum+sum/100*6;
 printf("Vasha summa sostavlyaet %d\n\n\n", den);                       
+}
+if(sr<366 && 241<=sr)
+{  
+den=sum+sum/100*12;
+printf("Vasha summa sostavlyaet %d\n\n\n", den);                       
 }   		
 }
 return 0;
